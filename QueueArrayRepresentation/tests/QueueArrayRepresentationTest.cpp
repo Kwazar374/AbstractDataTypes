233a94f34bcbef89fa35c4f0ea4a93da1240b1ce
Kwazar374 <66804584+Kwazar374@users.noreply.github.com>
2025-02-05 11:49:03 +0100

    Run first test on QueueArrayRepresentation

--- QueueArrayRepresentation/tests/QueueArrayRepresentationTest.cpp
+++ QueueArrayRepresentation/tests/QueueArrayRepresentationTest.cpp
@@ -1,6 +1,25 @@
 #include <gtest/gtest.h>
+#include <QueueArrayRepresentation.hpp>
 
-TEST(QueueArrayRepresentation, BasicAssertions) {
-    EXPECT_STRNE("hello", "world");
-    EXPECT_EQ(7 * 6, 42);
+// Constructor and IsEmpty()
+TEST(QueueArrayRepresentationTest, ConstructorAndIsEmpty) {
+    QueueArrayRepresentation<int> queue;
+    EXPECT_TRUE(queue.IsEmpty());
+    EXPECT_EQ(queue.Size(), 0);
 }
+
+// Enque() and Size()
+
+// Deque() and order of returned values
+
+// Front() and Back()
+
+// Deque() on empty queue
+
+// Copy Constructor and assignment operator
+
+// Resize when queue is full (there's no place at the end)
+
+// Clear()
+
+// ostream operator<<
